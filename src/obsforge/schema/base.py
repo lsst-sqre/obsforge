bbf0211d2cdad5474d91bebb4c2bd1958dcae052
Angelo Fausti <angelofausti@gmail.com>
2026-07-24 14:47:58 -0700

    Move ivoa schema to live next to ObsCore

--- src/obsforge/schema/base.py
+++ src/obsforge/schema/base.py
@@ -2,7 +2,7 @@
 
 from typing import ClassVar
 
-from sqlalchemy import DDL, Text, event
+from sqlalchemy import Text
 from sqlalchemy.orm import DeclarativeBase
 
 __all__ = ["SchemaBase"]
@@ -12,10 +12,3 @@ class SchemaBase(DeclarativeBase):
     """SQLAlchemy declarative base for the ObsForge database schema."""
 
     type_annotation_map: ClassVar = {str: Text()}
-
-
-event.listen(
-    SchemaBase.metadata,
-    "before_create",
-    DDL("CREATE SCHEMA IF NOT EXISTS ivoa"),
-)
